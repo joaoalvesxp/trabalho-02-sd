618328aa0593b4279c598c2592943c9577b38f1c
joaoalvesxp <joaoalvesxp@alu.ufc.br>
2022-12-13 00:45:08 -0300

    RMI Conclído, Objetos salvos apenas em Run Time.

--- TV.java
+++ TV.java
@@ -1,7 +1,7 @@
 import java.io.Serializable;
 import java.rmi.RemoteException;
 
-public class TV extends Aparelho{
+public class TV extends Aparelho {
     public TV(String nome, Double preço, int quantidadeNoEstoque, boolean transferivel) {
         super(nome, preço, quantidadeNoEstoque, transferivel);
     }
