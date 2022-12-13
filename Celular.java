8995d9494aaa034dedcc641d8a269e629e0b2924
joaoalvesxp <joaoalvesxp@alu.ufc.br>
2022-12-13 00:45:38 -0300

    RMI Conclído, Objetos salvos apenas em Run Time.

--- Celular.java
+++ Celular.java
@@ -2,6 +2,6 @@ import java.rmi.RemoteException;
 
 public class Celular extends Aparelho {
     public Celular(String nome, Double preço, int quantidadeNoEstoque, boolean transferivel) {
-        super(nome, preço, quantidadeNoEstoque, transferivel);
+         super(nome, preço, quantidadeNoEstoque, transferivel);
     }
 }
